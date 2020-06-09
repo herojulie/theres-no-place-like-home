c1a1daea5d7ba8a094d4083dfe05c98b435e5f24
herojulie <herojulie@gmail.com>
2020-06-09 09:29:30 -0400

    refresh date

--- src/component/BeenXDays.js
+++ src/component/BeenXDays.js
@@ -2,18 +2,18 @@ import React from 'react';
 import '../css/DefaultComponent.css';
 
 const getElapsedDays = () => {
-  let start = new Date("10/06/2019").getTime();
+  let start = new Date("06/08/2020").getTime();
   let today = Date.now();
 
   return Math.floor((today - start) / (24 * 60 * 60 * 1000));
-}
+};
 
 const beenXDays = () => {
   let elapsed = getElapsedDays()
 
   return (
     <div className="defaultComponent">
-      <h1>{elapsed} days</h1> 
+      <h1>{elapsed} days</h1>
       <p>!Drinking</p>
     </div>
   )
